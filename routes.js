609d3138df4d25d638573a7738d9b65b45cd86eb
KIPEK-ui <kipngetiche376@gmail.com>
2024-11-15 00:23:08 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(uploadDir)) {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'tmp/');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
